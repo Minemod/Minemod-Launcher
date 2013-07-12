6bd333d62313be91175c673179775fc0233eb292
cammygames <multicraftserver@ymail.com>
2013-07-12 09:35:27 +0100

    Update NetworkManager.java

--- src/org/minemod/dev/network/NetworkManager.java
+++ src/org/minemod/dev/network/NetworkManager.java
@@ -1,6 +1,8 @@
 package org.minemod.dev.network;
 
 import org.minemod.dev.gui.Launcher_Main;
+import java.io.*;
+import java.util.*;
 
 public class NetworkManager
 {
@@ -18,10 +20,23 @@ public class NetworkManager
 	}
 	
 	
-	public void connect()
+	/*
+	* I need to write a proper connet method that doesnt pass the username and pass through this files
+	* as it may be open to exploits.
+	*/
+	public void connect(int port,String server)
 	{
-		int port;
-		String server;
+		System.out.println(server + ":" + port)
+				
+	}
+	
+	/*
+	* This function will ping the server to check if the logins are running.
+	* maybe find a way to hook into the new MC launcher with their login checking and auth?
+	*/
+	public void ping(String server, int port) throws UnknownHostException, IOException
+	{
 		
 	}
+	
 }
