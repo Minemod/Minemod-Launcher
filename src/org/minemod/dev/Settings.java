fbfd2ed6f701f93eab038fdbb03065d3cc19cee9
Louis <admin@minemod.org>
2013-07-23 13:50:03 +0100

    added settings
    MineMod custom settings for URLs

---
+++ src/org/minemod/dev/Settings.java
@@ -0,0 +1,12 @@
+package org.minemod.dev;
+
+public class Settings {
+	
+	public static String getDomain() {
+		return "http://uk1.minemod.org";
+	}
+	
+	public static String getChangelogURL() {
+		return getDomain() + "/launcher/changelog.txt";
+	}
+}
